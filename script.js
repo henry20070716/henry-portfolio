295dba20885502b87646345a8f9fef7f41d2d3ed
henry20070716 <henryosfs@gmail.com>
2026-06-09 12:16:55 +0530

    Add files via upload
    A responsive personal portfolio website built using HTML and CSS to showcase my skills, projects, GitHub profile, and professional background as a Computer Science Engineering student at Loyola ICAM College of Engineering and Technology (LICET).

---
+++ script.js
@@ -0,0 +1,32 @@
+const texts = [
+  "Web Developer",
+  "Problem Solver",
+  "CSE Student",
+  "Tech Enthusiast"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+
+  if (count === texts.length) {
+    count = 0;
+  }
+
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.getElementById("typing").textContent = letter;
+
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+    setTimeout(type, 1200);
+  } else {
+    setTimeout(type, 100);
+  }
+
+})();
